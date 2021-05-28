4e610ffd50900c8dcc13cb0055c6b7fd94312f29
shixinyue <2549361568@qq.com>
2021-05-28 16:19:18 +0800

    practice

---
+++ js/practice.js
@@ -0,0 +1,10 @@
+
+const someFood = ['banana', 'apple', 'orange'];
+if(someFood.indexOf('banana')) {
+    console.log('i have to buy some banana');
+} else {
+    console.log('no need');
+} // ne need
+// why? -> indexOf返回的下标 0， 0是false
+
+
